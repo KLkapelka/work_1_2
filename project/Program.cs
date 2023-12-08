c7746728efbe37361e726b63e5e4e458e99de152
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 05:18:29 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,33 @@
+﻿// Добавляйте новые числа в список до тех пор, пока пользователь не введет 0.
+// Выведите в консоль сумму и произведение всех элементов списка.
+// Выведите средннее, среди всех элементов списка.
+
+using System;
+using System.Collections.Generic;
+class work_1_2
+{
+    static void Main()
+    {
+        List<float> array = new List<float>();
+        float sum = 0; 
+        float multiplication = 1; 
+        
+        while (true) 
+        {
+            Console.Write("Введите число: "); 
+            float number = float.Parse(Console.ReadLine()); 
+            if (number == 0)
+            {
+                break;
+            }
+            sum += number;  
+            multiplication *= number; 
+            array.Add(number); 
+        }
+        float average = sum / array.Count; 
+        
+        var str = string.Join(" ", array);        
+        Console.WriteLine(str);
+        Console.WriteLine($"Сумма: {sum} \t Произведение: {multiplication} \t Среднее значение: {average}");
+    }
+} 
